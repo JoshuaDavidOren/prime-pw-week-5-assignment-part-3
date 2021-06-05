4b53c78ecbfb3b2d92bec0e93b16103af842eabf
Joshua Oren <joshuadavidoren@gmail.com>
2021-06-05 11:21:10 -0500

    function showC created

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -8,5 +8,16 @@ function addToCollection(title, artist, yearPublished) {
   }
 }
 
-addToCollection('my', 'first', 1990)
-addToCollection('my2', 'first2', 19902)
+addToCollection('I Hate Everything About You', 'Three Days Grace', 2007)
+addToCollection('I Hate Everything About You', 'Longshot', 2019)
+addToCollection('Numb', 'Linkin Park', 2003)
+addToCollection('Numb', 'Aaron Richards, Mr FijiWiji', 2020)
+addToCollection('Oh Raven', 'Unlike pluto', 2019)
+addToCollection('Yes Offense', 'Unlike pluto', 2019)
+
+console.log('This is my collection:', collection);
+
+function showCollection(collection) {
+  console.log(collection.length);
+}
+console.log(showCollection(collection));
