0d9ab19aff24d5fbbc4df38eb8d4186eed65ccfe
Joshua Oren <joshuadavidoren@gmail.com>
2021-06-12 16:33:28 -0500

    fixedreturns, and formating

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -3,8 +3,10 @@ console.log('***** Music Collection *****')
 const collection = []
 
 function addToCollection(title, artist, yearPublished) {
-  collection.push({title, artist, yearPublished}); {
-    return console.log('New record:',({title, artist, yearPublished}));
+  collection.push({
+    title,
+    artist,
+    yearPublished}); {
   }
 }
 
@@ -18,21 +20,30 @@ addToCollection('Yes Offense', 'Unlike Pluto', 2019)
 console.log('This is my collection:', collection);
 
 function showCollection(collection) {
-  console.log('Here is my collection of',collection.length, 'records');{
-    for (let i = 0; i < collection.length; i++)
+  console.log('Here is my collection of',collection.length, 'records');
+
+  for (let i = 0; i < collection.length; i++) {
     console.log('TITLE:', collection[i].title, 'by', collection[i].artist, 'published in year', collection[i].yearPublished);
   }
-  return console.log('End of collection');
+  return true
+  console.log('End of collection');
 }
 console.log(showCollection(collection));
 
 function findByArtist(artist) {
   console.log('Searching inventory');
+
   let searchResults = []
     for (let i =0; i < collection.length; i++){
-      if (collection[i].artist === artist) searchResults.push([collection[i].title, collection[i].artist,  collection[i].yearPublished])
+      if (collection[i].artist === artist)
+      searchResults.push([
+        collection[i].title,
+        collection[i].artist,
+        collection[i].yearPublished
+      ]);
     }
- return console.log(searchResults);
+    return searchResults;
+ console.log(searchResults);
 }
 
 console.log(findByArtist('Unlike Pluto'));
